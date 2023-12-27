8881016608a397c15e62a565971376aad569a029
bautsi <b3r1a3dtw@gmail.com>
2023-12-28 01:34:40 +0800

    剩order

--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/OrderHelper.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/OrderHelper.java
@@ -1,240 +1,180 @@
 package ncu.im3069.demo.app;
 
 import java.sql.*;
-import java.util.*;
-
 import org.json.*;
 
 import ncu.im3069.demo.util.DBMgr;
 
 public class OrderHelper {
-    
+
     private static OrderHelper oh;
-    private Connection conn = null;
-    private PreparedStatement pres = null;
-    private OrderItemHelper oph =  OrderItemHelper.getHelper();
-    
+
     private OrderHelper() {
     }
-    
+
     public static OrderHelper getHelper() {
-        if(oh == null) oh = new OrderHelper();
-        
+        if (oh == null)
+            oh = new OrderHelper();
         return oh;
     }
-    
-    public JSONObject create(Order order) {
-        /** 記錄實際執行之SQL指令 */
-        String exexcute_sql = "";
-        long id = -1;
-        JSONArray opa = new JSONArray();
-        
-        try {
-            /** 取得資料庫之連線 */
-            conn = DBMgr.getConnection();
-            /** SQL指令 */
-            String sql = "INSERT INTO `missa`.`orders`(`last_name`, `first_name`, `email`, `address`, `phone`, `create`, `modify`)"
-                    + " VALUES(?, ?, ?, ?, ?, ?, ?)";
-            
-            /** 取得所需之參數 */
-            String first_name = order.getFirstName();
-            String last_name = order.getLastName();
-            String email = order.getEmail();
-            String address = order.getAddress();
-            String phone = order.getPhone();
-            Timestamp create = order.getCreateTime();
-            Timestamp modify = order.getModifyTime();
-            
-            /** 將參數回填至SQL指令當中 */
-            pres = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            pres.setString(1, last_name);
-            pres.setString(2, first_name);
-            pres.setString(3, email);
-            pres.setString(4, address);
-            pres.setString(5, phone);
-            pres.setTimestamp(6, create);
-            pres.setTimestamp(7, modify);
-            
-            /** 執行新增之SQL指令並記錄影響之行數 */
-            pres.executeUpdate();
-            
-            /** 紀錄真實執行的SQL指令，並印出 **/
-            exexcute_sql = pres.toString();
-            System.out.println(exexcute_sql);
-            
-            ResultSet rs = pres.getGeneratedKeys();
 
+    public JSONObject addOrder(int managerId, String productName, String productLocation, int quantity) {
+        JSONObject result = new JSONObject();
+        Connection conn = null;
+        CallableStatement cstmt = null;
+        try {
+            conn = DBMgr.getConnection();
+            // 確保調用的存儲過程名稱與您提供的存儲過程相匹配
+            cstmt = conn.prepareCall("{CALL sp_add_order1(?, ?, ?, ?)}");
+
+            cstmt.setInt(1, managerId);
+            cstmt.setString(2, productName);
+            cstmt.setString(3, productLocation);
+            cstmt.setInt(4, quantity);
+
+            ResultSet rs = cstmt.executeQuery();
             if (rs.next()) {
-                id = rs.getLong(1);
-                ArrayList<OrderItem> opd = order.getOrderProduct();
-                opa = oph.createByList(id, opd);
+                result.put("result", rs.getString("result"));
             }
         } catch (SQLException e) {
-            /** 印出JDBC SQL指令錯誤 **/
-            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            /** 若錯誤則印出錯誤訊息 */
             e.printStackTrace();
+            result.put("error", e.getMessage());
         } finally {
-            /** 關閉連線並釋放所有資料庫相關之資源 **/
-            DBMgr.close(pres, conn);
+            DBMgr.close(cstmt, conn);
         }
-
-        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
-        JSONObject response = new JSONObject();
-        response.put("order_id", id);
-        response.put("order_product_id", opa);
-
-        return response;
+        return result;
     }
-    
-    public JSONObject getAll() {
-        Order o = null;
-        JSONArray jsa = new JSONArray();
-        /** 記錄實際執行之SQL指令 */
-        String exexcute_sql = "";
-        /** 紀錄程式開始執行時間 */
-        long start_time = System.nanoTime();
-        /** 紀錄SQL總行數 */
-        int row = 0;
-        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
-        ResultSet rs = null;
-        
+
+    public JSONObject packOrder(int orderId) {
+        JSONObject result = new JSONObject();
+        Connection conn = null;
+        CallableStatement cstmt = null;
         try {
-            /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
-            /** SQL指令 */
-            String sql = "SELECT * FROM `missa`.`orders`";
-            
-            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
-            pres = conn.prepareStatement(sql);
-            /** 執行查詢之SQL指令並記錄其回傳之資料 */
-            rs = pres.executeQuery();
+            cstmt = conn.prepareCall("{CALL sp_pack_order(?)}");
 
-            /** 紀錄真實執行的SQL指令，並印出 **/
-            exexcute_sql = pres.toString();
-            System.out.println(exexcute_sql);
-            
-            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
-            while(rs.next()) {
-                /** 每執行一次迴圈表示有一筆資料 */
-                row += 1;
-                
-                /** 將 ResultSet 之資料取出 */
-                int id = rs.getInt("id");
-                String first_name = rs.getString("first_name");
-                String last_name = rs.getString("last_name");
-                String email = rs.getString("email");
-                String address = rs.getString("address");
-                String phone = rs.getString("phone");
-                Timestamp create = rs.getTimestamp("create");
-                Timestamp modify = rs.getTimestamp("modify");
-                
-                /** 將每一筆商品資料產生一名新Product物件 */
-                o = new Order(id, first_name, last_name, email, address, phone, create, modify);
-                /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
-                jsa.put(o.getOrderAllInfo());
+            cstmt.setInt(1, orderId);
+
+            boolean hasResultSet = cstmt.execute();
+            if (hasResultSet) {
+                ResultSet rs = cstmt.getResultSet();
+                if (rs.next()) {
+                    result.put("result", rs.getString("result"));
+                }
             }
-
         } catch (SQLException e) {
-            /** 印出JDBC SQL指令錯誤 **/
-            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            /** 若錯誤則印出錯誤訊息 */
             e.printStackTrace();
+            result.put("error", e.getMessage());
         } finally {
-            /** 關閉連線並釋放所有資料庫相關之資源 **/
-            DBMgr.close(rs, pres, conn);
+            DBMgr.close(cstmt, conn);
         }
-        
-        /** 紀錄程式結束執行時間 */
-        long end_time = System.nanoTime();
-        /** 紀錄程式執行時間 */
-        long duration = (end_time - start_time);
-        
-        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
-        JSONObject response = new JSONObject();
-        response.put("sql", exexcute_sql);
-        response.put("row", row);
-        response.put("time", duration);
-        response.put("data", jsa);
-
-        return response;
+        return result;
     }
-    
-    public JSONObject getById(String order_id) {
-        JSONObject data = new JSONObject();
-        Order o = null;
-        /** 記錄實際執行之SQL指令 */
-        String exexcute_sql = "";
-        /** 紀錄程式開始執行時間 */
-        long start_time = System.nanoTime();
-        /** 紀錄SQL總行數 */
-        int row = 0;
-        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
-        ResultSet rs = null;
-        
+
+    public JSONArray viewOrders() {
+        JSONArray orders = new JSONArray();
+        Connection conn = null;
+        CallableStatement cstmt = null;
         try {
-            /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
-            /** SQL指令 */
-            String sql = "SELECT * FROM `missa`.`orders` WHERE `orders`.`id` = ?";
-            
-            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
-            pres = conn.prepareStatement(sql);
-            pres.setString(1, order_id);
-            /** 執行查詢之SQL指令並記錄其回傳之資料 */
-            rs = pres.executeQuery();
+            cstmt = conn.prepareCall("{CALL sp_view_orders()}");
 
-            /** 紀錄真實執行的SQL指令，並印出 **/
-            exexcute_sql = pres.toString();
-            System.out.println(exexcute_sql);
-            
-            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
-            while(rs.next()) {
-                /** 每執行一次迴圈表示有一筆資料 */
-                row += 1;
-                
-                /** 將 ResultSet 之資料取出 */
-                int id = rs.getInt("id");
-                String first_name = rs.getString("first_name");
-                String last_name = rs.getString("last_name");
-                String email = rs.getString("email");
-                String address = rs.getString("address");
-                String phone = rs.getString("phone");
-                Timestamp create = rs.getTimestamp("create");
-                Timestamp modify = rs.getTimestamp("modify");
-                
-                /** 將每一筆商品資料產生一名新Product物件 */
-                o = new Order(id, first_name, last_name, email, address, phone, create, modify);
-                /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
-                data = o.getOrderAllInfo();
+            ResultSet rs = cstmt.executeQuery();
+            while (rs.next()) {
+                JSONObject order = new JSONObject();
+                order.put("order_id", rs.getInt("order_id"));
+                order.put("manager_id", rs.getInt("manager_id"));
+                order.put("product_name", rs.getString("orders_product_name"));
+                order.put("product_price", rs.getDouble("orders_product_price"));
+                order.put("product_quantity", rs.getInt("orders_product_quantity"));
+                order.put("packed_time", rs.getTimestamp("orders_packed_time"));
+                order.put("statement", rs.getString("orders_statement"));
+                orders.put(order);
             }
-
         } catch (SQLException e) {
-            /** 印出JDBC SQL指令錯誤 **/
-            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
-        } catch (Exception e) {
-            /** 若錯誤則印出錯誤訊息 */
             e.printStackTrace();
         } finally {
-            /** 關閉連線並釋放所有資料庫相關之資源 **/
-            DBMgr.close(rs, pres, conn);
+            DBMgr.close(cstmt, conn);
         }
-        
-        /** 紀錄程式結束執行時間 */
-        long end_time = System.nanoTime();
-        /** 紀錄程式執行時間 */
-        long duration = (end_time - start_time);
-        
-        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
-        
-        JSONObject response = new JSONObject();
-        response.put("sql", exexcute_sql);
-        response.put("row", row);
-        response.put("time", duration);
-        response.put("data", data);
-
-        return response;
+        return orders;
     }
+
+    public JSONObject deleteOrder(int orderId) {
+        JSONObject result = new JSONObject();
+        Connection conn = null;
+        CallableStatement cstmt = null;
+        try {
+            conn = DBMgr.getConnection();
+            cstmt = conn.prepareCall("{CALL sp_delete_order(?)}");
+
+            cstmt.setInt(1, orderId);
+
+            ResultSet rs = cstmt.executeQuery();
+            if (rs.next()) {
+                result.put("result", rs.getString("result"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            result.put("error", e.getMessage());
+        } finally {
+            DBMgr.close(cstmt, conn);
+        }
+        return result;
+    }
+
+    public JSONObject updateOrder(int orderId, int newQuantity) {
+        JSONObject result = new JSONObject();
+        Connection conn = null;
+        CallableStatement cstmt = null;
+        try {
+            conn = DBMgr.getConnection();
+            cstmt = conn.prepareCall("{CALL sp_update_orders(?, ?)}");
+
+            cstmt.setInt(1, orderId);
+            cstmt.setInt(2, newQuantity);
+
+            ResultSet rs = cstmt.executeQuery();
+            if (rs.next()) {
+                result.put("result", rs.getString("result"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            result.put("error", e.getMessage());
+        } finally {
+            DBMgr.close(cstmt, conn);
+        }
+        return result;
+    }
+
+    public JSONObject generateSalesReport() {
+        JSONObject result = new JSONObject();
+        JSONArray salesReport = new JSONArray();
+        Connection conn = null;
+        CallableStatement cstmt = null;
+        try {
+            conn = DBMgr.getConnection();
+            cstmt = conn.prepareCall("{CALL sp_generate_sales_report()}");
+
+            ResultSet rs = cstmt.executeQuery();
+            while (rs.next()) {
+                JSONObject reportItem = new JSONObject();
+                reportItem.put("product_name", rs.getString("product_name"));
+                reportItem.put("total_quantity", rs.getInt("total_quantity"));
+                reportItem.put("unit_price", rs.getDouble("unit_price"));
+                reportItem.put("total_sales", rs.getDouble("total_sales"));
+                salesReport.put(reportItem);
+            }
+            result.put("status", "200");
+            result.put("message", "Sales report generated successfully");
+            result.put("response", salesReport);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            result.put("status", "500");
+            result.put("error", e.getMessage());
+        } finally {
+            DBMgr.close(cstmt, conn);
+        }
+        return result;
+    }
+
 }
--- ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/OrderController.java
+++ ncu-mis-sa-master/NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/OrderController.java
@@ -5,114 +5,81 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import org.json.*;
 
-import ncu.im3069.demo.app.Order;
-import ncu.im3069.demo.app.Product;
-import ncu.im3069.demo.app.ProductHelper;
 import ncu.im3069.demo.app.OrderHelper;
 import ncu.im3069.tools.JsonReader;
-
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/api/order.do")
 public class OrderController extends HttpServlet {
 
-    /** The Constant serialVersionUID. */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private OrderHelper oh = OrderHelper.getHelper();
 
-    /** ph，ProductHelper 之物件與 Product 相關之資料庫方法（Sigleton） */
-    private ProductHelper ph =  ProductHelper.getHelper();
-
-    /** oh，OrderHelper 之物件與 order 相關之資料庫方法（Sigleton） */
-	private OrderHelper oh =  OrderHelper.getHelper();
-
-    /**
-     * 處理 Http Method 請求 GET 方法（新增資料）
-     *
-     * @param request Servlet 請求之 HttpServletRequest 之 Request 物件（前端到後端）
-     * @param response Servlet 回傳之 HttpServletResponse 之 Response 物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        /** 透過 JsonReader 類別將 Request 之 JSON 格式資料解析並取回 */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         JsonReader jsr = new JsonReader(request);
+        String action = jsr.getParameter("action");
 
-        /** 取出經解析到 JsonReader 之 Request 參數 */
-        String id = jsr.getParameter("id");
-
-        /** 新建一個 JSONObject 用於將回傳之資料進行封裝 */
         JSONObject resp = new JSONObject();
-
-        /** 判斷該字串是否存在，若存在代表要取回個別訂單之資料，否則代表要取回全部資料庫內訂單之資料 */
-        if (!id.isEmpty()) {
-          /** 透過 orderHelper 物件的 getByID() 方法自資料庫取回該筆訂單之資料，回傳之資料為 JSONObject 物件 */
-          JSONObject query = oh.getById(id);
-          resp.put("status", "200");
-          resp.put("message", "單筆訂單資料取得成功");
-          resp.put("response", query);
-        }
-        else {
-          /** 透過 orderHelper 物件之 getAll() 方法取回所有訂單之資料，回傳之資料為 JSONObject 物件 */
-          JSONObject query = oh.getAll();
-          resp.put("status", "200");
-          resp.put("message", "所有訂單資料取得成功");
-          resp.put("response", query);
+        if ("viewAll".equals(action)) {
+            JSONArray orders = oh.viewOrders();
+            resp.put("status", "200");
+            resp.put("message", "所有訂單資料取得成功");
+            resp.put("response", orders);
         }
 
-        /** 透過 JsonReader 物件回傳到前端（以 JSONObject 方式） */
         jsr.response(resp, response);
-	}
+    }
 
-    /**
-     * 處理 Http Method 請求 POST 方法（新增資料）
-     *
-     * @param request Servlet 請求之 HttpServletRequest 之 Request 物件（前端到後端）
-     * @param response Servlet 回傳之 HttpServletResponse 之 Response 物件（後端到前端）
-     * @throws ServletException the servlet exception
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    /** 透過 JsonReader 類別將 Request 之 JSON 格式資料解析並取回 */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         JsonReader jsr = new JsonReader(request);
         JSONObject jso = jsr.getObject();
+        String action = jso.optString("action");
 
-        /** 取出經解析到 JSONObject 之 Request 參數 */
-        String first_name = jso.getString("first_name");
-        String last_name = jso.getString("last_name");
-        String email = jso.getString("email");
-        String address = jso.getString("address");
-        String phone = jso.getString("phone");
-        JSONArray item = jso.getJSONArray("item");
-        JSONArray quantity = jso.getJSONArray("quantity");
+        JSONObject resp = new JSONObject();
+        if ("addOrder".equals(action)) {
+            int managerId = jso.getInt("manager_id");
+            String productName = jso.getString("product_name");
+            String productLocation = jso.getString("product_location"); // 新增的参数
+            int quantity = jso.getInt("quantity");
 
-        /** 建立一個新的訂單物件 */
-        Order od = new Order(first_name, last_name, email, address, phone);
+            // 确保传递所有必要的参数给 addOrder 方法
+            JSONObject result = oh.addOrder(managerId, productName, productLocation, quantity);
+            resp.put("status", "200");
+            resp.put("message", "訂單新增成功");
+            resp.put("response", result);
+        } else if ("packOrder".equals(action)) {
+            int orderId = jso.getInt("order_id");
 
-        /** 將每一筆訂單細項取得出來 */
-        for(int i=0 ; i < item.length() ; i++) {
-            String product_id = item.getString(i);
-            int amount = quantity.getInt(i);
+            JSONObject result = oh.packOrder(orderId);
+            resp.put("status", "200");
+            resp.put("message", "訂單打包成功");
+            resp.put("response", result);
+        } else if ("generateSalesReport".equals(action)) {
+            JSONObject report = oh.generateSalesReport();
+            resp.put("status", "200");
+            resp.put("message", "销售报告生成成功");
+            resp.put("response", report);
+        } else if ("deleteOrder".equals(action)) {
+            int orderId = jso.getInt("order_id");
 
-            /** 透過 ProductHelper 物件之 getById()，取得產品的資料並加進訂單物件裡 */
-            Product pd = ph.getById(product_id);
-            od.addOrderProduct(pd, amount);
+            JSONObject result = oh.deleteOrder(orderId);
+            resp.put("status", "200");
+            resp.put("message", "訂單刪除成功");
+            resp.put("response", result);
+        }else if ("updateOrder".equals(action)) {
+            int orderId = jso.getInt("order_id");
+            int newQuantity = jso.getInt("new_quantity");
+
+            JSONObject result = oh.updateOrder(orderId, newQuantity);
+            resp.put("status", "200");
+            resp.put("message", "訂單更新成功");
+            resp.put("response", result);
         }
 
-        /** 透過 orderHelper 物件的 create() 方法新建一筆訂單至資料庫 */
-        JSONObject result = oh.create(od);
-
-        /** 設定回傳回來的訂單編號與訂單細項編號 */
-        od.setId((int) result.getLong("order_id"));
-        od.setOrderProductId(result.getJSONArray("order_product_id"));
-
-        /** 新建一個 JSONObject 用於將回傳之資料進行封裝 */
-        JSONObject resp = new JSONObject();
-        resp.put("status", "200");
-        resp.put("message", "訂單新增成功！");
-        resp.put("response", od.getOrderAllInfo());
-
-        /** 透過 JsonReader 物件回傳到前端（以 JSONObject 方式） */
         jsr.response(resp, response);
-	}
-
+    }
 }
